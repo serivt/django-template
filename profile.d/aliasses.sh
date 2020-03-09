560d3621c575375a83896b06d2993d6852ae525e
Sergio Torrado <sergio.itp16@gmail.com>
2020-03-09 15:24:56 -0500

    Closes #1 -- Ajustar según estilo de codificación

--- profile.d/aliasses.sh
+++ profile.d/aliasses.sh
@@ -2,3 +2,13 @@ alias runserver="poetry run python manage.py runserver 0.0.0.0:8000"
 alias makemigrations="poetry run python manage.py makemigrations"
 alias migrate="poetry run python manage.py migrate"
 alias shell="poetry run python manage.py shell"
+alias black="poetry run black"
+alias isort="poetry run isort -rc"
+alias flake8="poetry run flake8"
+alias check="poetry run black flake8"
+
+check() {
+    poetry run isort -rc $1
+    poetry run black $1
+    poetry run flake8 $1
+}
